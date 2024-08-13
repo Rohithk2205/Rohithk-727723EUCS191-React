7eadc7a138916bdeb6731ff29e51816204037c6c
Rohithk2205 <146612497+Rohithk2205@users.noreply.github.com>
2024-08-13 09:14:54 +0530

    Add files via upload

---
+++ Day1/Day-one-cw-2.jsx
@@ -0,0 +1,16 @@
+
+function D1cw1
+{
+    let myobject ={name:"Madras"};
+    let newObject=myobject;
+    myobject.name="Chennai";
+
+    let myarray=["a","e","i","o",];
+    myarray.push("u");
+
+    console.log(newObject);
+    console.log(myarray);
+
+    alert("Check the console output");
+}
+export default D1cw1;
---
+++ Day1/Day-one-cw-3.jsx
@@ -0,0 +1,9 @@
+function World()
+{
+    return(
+<div>
+    <h1>This World is function component</h1>
+</div>
+    )
+};
+export default World;
